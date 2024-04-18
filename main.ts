7be17d518050f39c569229609b7a71e9504101f6
Leon Liu <liuleon360@gmail.com>
2024-04-17 23:31:50 -0700

    add parsing and validating HTTP/1.1 request and echo response with headers and body

--- main.ts
+++ main.ts
@@ -1,18 +1,139 @@
+/*
+const listener = Deno.listen({ port: 5000 });
+console.log("listening on 0.0.0.0:5000");
+for await (const conn of listener) {
+    conn.localAddr
+}
+*/
+import { concat, equals } from "https://deno.land/std@0.102.0/bytes/mod.ts";
+import { indexOfNeedle } from "https://deno.land/std@0.223.0/bytes/mod.ts";
+
+async function handleConnection(conn: Deno.Conn): Promise<void> { // get a connection out of the way
+    const buf = new Uint8Array(4096);
+    while (true) {
+        const nbytes = await conn.read(buf);
+        if (nbytes === null) {
+            break;
+        }
+
+        /**
+         * Sample request:
+            POST / HTTP/1.1
+            Host: 127.0.0.1:8080
+            User-Agent: curl/8.7.1
+            Accept: /
+            Content-Length: 15
+            Content-Type: application/x-www-form-urlencoded
+
+            a=b&asdf=foobar
+         */
+        console.log(nbytes);
+        const enc = new TextEncoder();
+        const dec = new TextDecoder();
+        
+        const B_CRLF = enc.encode("\r\n");
+        const B_SPACE = enc.encode(" ");
+        const firstCRLF = indexOfNeedle(buf, B_CRLF); // index of first CRLF
+        console.log("First line is: " + dec.decode(buf.slice(0, firstCRLF)));
+
+        // get method
+        const idxMethodEnd = indexOfNeedle(buf, B_SPACE); // index of first space
+        const method = buf.slice(0, idxMethodEnd);
+        const VALID_METHODS = ["GET", "POST", "PATCH", "OPTIONS", "HEAD", "CONNECT", "TRACE"];
+        if (!VALID_METHODS.includes(dec.decode(method))) {
+            console.log("Invalid method");
+            continue;
+        }
+        // console.log("Method: " + dec.decode(method));
+
+        // get request target
+        const idxRequestTargetEnd = indexOfNeedle(buf, B_SPACE, idxMethodEnd+1); 
+        if(idxRequestTargetEnd === -1) {
+            console.log("Missing request target");
+            continue;
+        }
+        // const requestTarget = buf.slice(idxMethodEnd+1, idxRequestTargetEnd);
+        // console.log("Request target: " + dec.decode(requestTarget));
+
+        //get http version 
+        //slice to end of line
+        const httpVersion = buf.slice(idxRequestTargetEnd+1, firstCRLF); 
+        const B_VALID_HTTP_VERSION = enc.encode("HTTP/1.1");
+        //check that it is HTTP/1.1
+        if(!equals(httpVersion, B_VALID_HTTP_VERSION)) {
+            console.log("Invalid HTTP version");
+            continue;
+        }   
+        // const decodedHttpVersion = dec.decode(httpVersion).trim();
+        // console.log(`[${decodedHttpVersion}]`, decodedHttpVersion.length); 
+    
+        
+        //get headers
+        
+        // create UInt8Array concatenating two B_CRLF
+        const B_HEADERS_END = concat(B_CRLF, B_CRLF);
+        //end of headers delimiter is two CRLF
+        const idxHeadersEnd = indexOfNeedle(buf, B_HEADERS_END);
+        const headers = [];
+        let idxHeaderStart = firstCRLF;
+
+        const B_COLON = enc.encode(":");
+        while(idxHeaderStart < idxHeadersEnd) {
+            const idxHeaderEnd = indexOfNeedle(buf, B_CRLF, idxHeaderStart+1);
+            const fieldLine = buf.slice(idxHeaderStart, idxHeaderEnd);
+
+            const colonIdx = indexOfNeedle(fieldLine, B_COLON);
+            if(colonIdx === -1) {
+                console.log("Invalid header at " + dec.decode(fieldLine) + " missing colon");
+                continue;
+            }
+            //field-line   = field-name ":" OWS field-value OWS
+            const fieldName = fieldLine.slice(0, colonIdx);
+            let fieldValue = fieldLine.slice(colonIdx+1);
+
+            //trim optional white space (OWD) on field value
+            fieldValue = enc.encode(dec.decode(fieldValue).trim());
+            
+            headers.push({fieldName, fieldValue});
+
+            //move header start to end of current header
+            idxHeaderStart = idxHeaderEnd + 2;
+        }
+
+        //get requestBody (remaining portion of request after headers)
+        const requestBody = buf.slice(idxHeadersEnd+4); 
 
 
-/**
- * echo_server.ts
- */
+        /* Write response */
+        let response = `${dec.decode(httpVersion)} 200 OK\r\n`;
+        for(const header of headers) {
+            response += `${dec.decode(header.fieldName)}: ${dec.decode(header.fieldValue)}\r\n`;
+        }
+        response += "\r\n";
+        response += dec.decode(requestBody);
+
+        // console.log(response);
+        
+        await conn.write(enc.encode(response))
+    }
+    conn.close();
+}
 
 const listener = Deno.listen({ port: 8080 });
-console.log("listening on 0.0.0.0:8080");
 
+// create an await for loop to handle all connections
 for await (const conn of listener) {
-    while(true) {    
-        const data = new Uint8Array(1024);
-        const bytesRead = await conn.read(data);
-        console.log("bytes read: ", bytesRead);
-        const bytesWritten = await conn.write(data);
-        console.log("bytes written: ", bytesWritten);
-    }
+    handleConnection(conn);
 }
+
+
+// parse response message
+
+// POST / HTTP/1.1
+// Host: 127.0.0.1:8080
+// User-Agent: curl/8.7.1
+// Accept: */*
+// Content-Length: 15
+// Content-Type: application/x-www-form-urlencoded
+
+// a=b&asdf=foobar
