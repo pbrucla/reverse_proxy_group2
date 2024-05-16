8526c1fb27608aafbd85a9c547d89eb7f3f0b53e
Kevin Wong <kevin.g.wong.05@gmail.com>
2024-05-15 19:13:40 -0700

    Change readPerm and writePerm to const.

--- logging.ts
+++ logging.ts
@@ -47,8 +47,8 @@ export async function log(logType: string, message: string, context: object = {}
     }
 
     // request r/w permissions if needed
-    let readPerm = Deno.permissions.requestSync({name: "read"});
-    let writePerm = Deno.permissions.requestSync({name: "write"});
+    const readPerm = Deno.permissions.requestSync({name: "read"});
+    const writePerm = Deno.permissions.requestSync({name: "write"});
 
     if (readPerm.state === "denied") console.log("Deno does not have permission to read. Logging may be impossible.\n");
     if (writePerm.state === "denied") console.log("deno does not have permission to write. Editing or creating new log filels may be impossible.\n")
